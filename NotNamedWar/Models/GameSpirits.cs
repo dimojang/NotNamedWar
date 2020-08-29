13b89493a304db3f8aa9762a72c2b07f9a8d5ce5
DIMOJANG <dimojang@outlook.com>
2020-08-29 21:16:59 +0800

    bugs fixed

--- NotNamedWar/Models/GameSpirits.cs
+++ NotNamedWar/Models/GameSpirits.cs
@@ -13,6 +13,8 @@ namespace NotNamedWar.Models
     {
         public List<Spirit> Spirits { get; set; } = new List<Spirit>();
 
+        private float[] rotations = new float[6] { 0, -(float)Math.PI / 3, -(float)Math.PI * 2 / 3, -(float)Math.PI, (float)Math.PI * 2 / 3, (float)Math.PI / 3 };
+
         public void DrawSpirits(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, GameMap gameMap)
         {
             Texture2D texture = new Texture2D(graphicsDevice, 1, 1, false, SurfaceFormat.Color);
@@ -31,23 +33,11 @@ namespace NotNamedWar.Models
                 int y = (int)gameMap.Position.Y + ((int)spirit.Position.Y - 1) * hexHeight * 3 / 4 + hexHeight / 4;
 
                 int l = spirit.Length == 1 ? 50 : ((spirit.Length - 2) * 2 + 2) * Length;
-
-                float rotation = new Func<float>(() =>
-                {
-                    switch (spirit.Direction)
-                    {
-                        case 1: return 0;
-                        case 2: return -(float)Math.PI / 3;
-                        case 3: return -(float)Math.PI * 2 / 3;
-                        case 4: return -(float)Math.PI;
-                        case 5: return (float)Math.PI * 2 / 3;
-                        case 6: return (float)Math.PI / 3;
-                        default: return 0;
-                    }
-                })();
-
+                
                 spriteBatch.Draw(texture, new Rectangle(x, y, l, spirit.Width), null,
-                    spirit.SpiritColor, rotation, new Vector2(0f, 0f), SpriteEffects.None, 1f);
+                    spirit.SpiritColor, rotations[spirit.Direction], new Vector2(0f, 0f), SpriteEffects.None, 1f);
+
+                spriteBatch.Draw(texture, new Rectangle(x, y, 10, 10), Color.Red);
             }
         }
     }
--- NotNamedWar/Models/Spirit.cs
+++ NotNamedWar/Models/Spirit.cs
@@ -11,12 +11,21 @@ namespace NotNamedWar.Models
     {
         public Color SpiritColor { get; set; } = Color.Red;
 
-        public int Direction { get; set; } = 1;
+        public int Direction { get; set; } = 0;
 
         public int Length { get; set; } = 2;
 
         public int Width { get; set; } = 1;
 
         public Vector2 Position { get; set; } = new Vector2(5, 5);
+
+        /// <summary>
+        /// Move Spirit Forward
+        /// </summary>
+        /// <param name="distance">distance to move</param>
+        public void MoveForward(int distance)
+        {
+
+        }
     }
 }
